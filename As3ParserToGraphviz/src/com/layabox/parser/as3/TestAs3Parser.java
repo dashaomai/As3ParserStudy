7af7c7031fa40f96e84ee507961f6e4e7556822f
Bob Jiang <dashaomai@gmail.com>
2016-07-28 11:25:50 +0800

    初步引入了 As3 语法树解析库源代码

---
+++ As3ParserToGraphviz/src/com/layabox/parser/as3/TestAs3Parser.java
@@ -0,0 +1,129 @@
+package com.layabox.parser.as3;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import com.adobe.ac.pmd.parser.IAS3Parser;
+import com.adobe.ac.pmd.parser.IParserNode;
+import com.adobe.ac.pmd.parser.NodeKind;
+import com.adobe.ac.pmd.parser.exceptions.TokenException;
+
+import de.bokelberg.flex.parser.AS3Parser;
+
+public class TestAs3Parser {
+
+	public static void main(String[] args) {
+		TestAs3Parser test = new TestAs3Parser();
+		
+		try {
+			test.Parse("test/examples/JPEGEncoder.as");
+		} catch (Exception ex) {
+			System.err.println(ex);
+		}
+	}
+
+    private void Parse(final String filePath) throws URISyntaxException, IOException, TokenException {
+        IAS3Parser parser = new AS3Parser();
+
+        String path = getClass().getResource(filePath).toURI().getPath();
+        IParserNode root = parser.buildAst(path);
+        StringBuffer buffer = new StringBuffer();
+
+        BeginVisit(buffer);
+        VisitNode(root, buffer, 1);
+        EndVisit(buffer);
+
+        System.out.println(buffer);
+    }
+
+    private static int currentStructId;
+
+    private static void BeginVisit(final StringBuffer buffer) {
+        currentStructId = 0;
+
+        buffer.setLength(0);
+        buffer.append("digraph structs {\n");
+        buffer.append("\tnode [shape=record];\n");
+    }
+
+    private static void EndVisit(final StringBuffer buffer) {
+        buffer.append("}\n");
+    }
+
+    private static void VisitNode(final IParserNode ast, final StringBuffer buffer, final int level) {
+        VisitNode(ast, buffer, level, 0);
+    }
+
+    private static void VisitNode(final IParserNode ast, final StringBuffer buffer, final int level, final int parentStructId) {
+
+        if (
+            ast.is(NodeKind.BLOCK) ||
+            ast.is(NodeKind.META_LIST) ||
+            ast.is(NodeKind.INIT)
+        )
+            return;
+
+        currentStructId++;
+
+        final int myId = currentStructId;
+
+        buffer.append("\t");
+        buffer.append("struct");
+        buffer.append(myId);
+        buffer.append(" [shape=record, label=\"");
+        buffer.append(ast.getId());
+        buffer.append("(");
+        buffer.append(level);
+        buffer.append(")");
+
+        final String stringVal = ast.getStringValue();
+        if (stringVal != null && !stringVal.equals("")) {
+            buffer.append("\\n");
+            buffer.append(EscapeEntities(stringVal));
+        }
+
+        buffer.append("\"];\n");
+
+        if (0 != parentStructId) {
+            buffer.append("\tstruct");
+            buffer.append(parentStructId);
+            buffer.append(" -> struct");
+            buffer.append(myId);
+            buffer.append(";\n");
+        }
+
+        if (level < 777) {
+            final int numChildren = ast.numChildren();
+            if (numChildren > 0) {
+                for (int i = 0; i<numChildren; i++) {
+                    VisitNode(ast.getChild(i), buffer, level + 1, myId);
+                }
+            }
+        }
+    }
+
+    private static String EscapeEntities( final String stringToEscape )
+    {
+        final StringBuffer buffer = new StringBuffer();
+
+        for ( int i = 0; i < stringToEscape.length(); i++ )
+        {
+            final char currentCharacter = stringToEscape.charAt( i );
+
+            if ( currentCharacter == '<' )
+            {
+                buffer.append( "&lt;" );
+            }
+            else if ( currentCharacter == '>' )
+            {
+                buffer.append( "&gt;" );
+            }
+            else
+            {
+                buffer.append( currentCharacter );
+            }
+        }
+        return buffer.toString();
+    }
+
+}
